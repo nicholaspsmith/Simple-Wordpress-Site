bc1fd6e68ca0576f301e979e16bfaeee110ac22e
nicholaspsmith <me@nicholaspsmith.com>
2014-06-30 08:35:33 -0500

    update link for bg image

--- wp-content/themes/nicholaspsmith/lib/scripts.php
+++ wp-content/themes/nicholaspsmith/lib/scripts.php
@@ -11,7 +11,7 @@
  * 3. /theme/assets/js/main.min.js (in footer)
  */
 function roots_scripts() {
-  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '19579b651f6e78f07bbd350e277c2cfa');
+  wp_enqueue_style('roots_main', get_template_directory_uri() . '/assets/css/main.min.css', false, '82cc2fb44be571c694b5a5cac10ab506');
 
   // jQuery is loaded using the same method from HTML5 Boilerplate:
   // Grab Google CDN's latest jQuery with a protocol relative URL; fallback to local if offline
